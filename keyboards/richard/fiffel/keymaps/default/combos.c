bb83dd6f97ea0c2b8e07f9cfa817b91d028b992c
richard8003 <richard.johansson.8003@gmail.com>
2024-01-23 10:14:26 +0100

    fubar

--- keyboards/richard/fiffel/keymaps/default/combos.c
+++ keyboards/richard/fiffel/keymaps/default/combos.c
@@ -5,9 +5,9 @@ const uint16_t PROGMEM copy[] = {KC_S, KC_T, COMBO_END};
 const uint16_t PROGMEM paste[] = {KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM single_quote[] = {KC_N, KC_O, COMBO_END};
 const uint16_t PROGMEM question_mark[] = {KC_U, KC_Y, COMBO_END};
-// const uint16_t PROGMEM flameshot[] = {KC_Q, KC_W, COMBO_END};
-// const uint16_t PROGMEM screenshot[] = {KC_F, KC_P, COMBO_END};
 const uint16_t PROGMEM bang[] = {KC_W, KC_F, COMBO_END};
+const uint16_t PROGMEM comma[] = {KC_2, KC_3, COMBO_END};
+const uint16_t PROGMEM fubar[] = {KC_E, KC_I, COMBO_END};
 
 
 combo_t key_combos[] = {
@@ -16,7 +16,8 @@ combo_t key_combos[] = {
     COMBO(single_quote, KC_BSLS), // keycodes with modifiers are possible too!
     COMBO(single_quote, KC_BSLS), // keycodes with modifiers are possible too!
     COMBO(question_mark, LSFT(KC_MINS)), // keycodes with modifiers are possible too!
-    // COMBO(flameshot, LGUI(LSFT(KC_X))), // keycodes with modifiers are possible too!
-    // COMBO(screenshot, LGUI(LSFT(KC_4))), // keycodes with modifiers are possible too!
     COMBO(bang, LSFT(KC_1)), // keycodes with modifiers are possible too!
+    COMBO(comma, KC_COMM), // keycodes with modifiers are possible too!
+    COMBO(fubar, LT(2,KC_NO)), // keycodes with modifiers are possible too!
+
 };
