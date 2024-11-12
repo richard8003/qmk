c17fc37fa0341e5e8230356091fbb2c2ca65b51a
richard8003 <richard.johansson.8003@gmail.com>
2024-11-12 23:29:01 +0100

    fubar

---
+++ keyboards/richard/fiffel_pcb_arduino/keymaps/fubar/definitions.c
@@ -0,0 +1,79 @@
+
+
+#define       NONE              KC_NO
+
+// arrowkeys
+#define       UPP               KC_UP
+#define       DOWN              KC_DOWN
+#define       LEFT              KC_LEFT
+#define       RIGHT             KC_RIGHT
+
+// layer1
+// #define       L1_BSPC           LT(1, KC_BSPC)
+// #define       L1_SPC            LT(1, KC_SPC)
+// #define       L2_CAPS           LT(2, KC_CAPS)
+// #define       L3_ESC            LT(3, KC_ESC)
+// #define       CMD_TAB           LGUI_T(KC_TAB)
+// #define       CTL_ENT           RCTL_T(KC_ENT)
+// #define       OPT_ESC           LALT_T(KC_ESC)
+// #define       SRCSHOT           TD(TD_SCRSHT)
+// #define       FLAMESHOT         TD(TD_SLNGSHT)
+
+// layer2
+#define       PIPE              LALT(KC_7)
+#define       AT                RALT(KC_2)
+#define       AND               LSFT(KC_6)
+#define       SLASH             LSA(KC_7)
+#define       TILDE             RALT(KC_RBRC)
+#define       PLUS              KC_PPLS
+#define       BANG              KC_EXLM
+#define       QUO               LSFT(KC_2)
+#define       HASH              KC_HASH
+#define       DOLLAR            RALT(KC_4)
+#define       PROCENT           LSFT(KC_5)
+#define       EQUAL             KC_PEQL
+#define       ODD_QUO           KC_PLUS
+#define       LESS              KC_GRV
+#define       MORE              KC_TILD
+#define       HYPH              LSFT(KC_SLSH)
+#define       LOW_HYPH          KC_SLSH
+#define       TIMES             KC_PIPE,
+
+// layer3
+#define       L4                LT(4,NONE)
+#define       DESK1             LGUI(KC_1)
+#define       DESK2             LGUI(KC_2)
+#define       DESK3             LGUI(KC_3)
+#define       YABAI_NAV_UP      LAG(KC_U)
+#define       YABAI_NAV_DOWN    LAG(KC_E)
+#define       YABAI_NAV_LEFT    LAG(KC_N)
+#define       YABAI_NAV_RIGHT   LAG(KC_I)
+#define       YABAI_FLOAT       LAG(KC_Y)
+#define       YABAI_FULL_SRC    LAG(KC_Y)
+#define       ZOOM_OUT          LGUI(KC_SLSH)
+#define       ZOOM_IN           LGUI(KC_MINS)
+#define       SPOTIFY           LALT(KC_U)
+#define       TERMINAL          LALT(KC_ENT)
+#define       CHROME            LALT(KC_SPC)
+#define       DISCORD           LALT(KC_D)
+#define       CMD_K             LGUI(KC_K),
+
+#define       PREV_TRACK        KC_MPRV
+#define       VOL_DOWN          KC_VOLD
+#define       PLAY_PAUS         KC_MPLY
+#define       VOL_UP            KC_VOLU
+#define       NEXT_TRACK        KC_MNXT
+
+#define       YABAI_MOVE_SRC1   LAG(KC_1)
+#define       YABAI_MOVE_SRC2   LAG(KC_2)
+#define       YABAI_MOVE_SRC3   LAG(KC_3)
+
+#define       YABAI_LAYOUT      LAG(KC_SPC)
+#define       BROWS_TAB         LCTL(LSFT(KC_TAB))
+#define       BROWS_SHIFT_TAB   LCTL(KC_TAB)
+
+#define       YABAI_MOV_UP      LAG(KC_F)
+#define       YABAI_MOV_DOWN    LAG(KC_S)
+#define       YABAI_MOV_LEFT    LAG(KC_R)
+#define       YABAI_MOV_RIGHT   LAG(KC_T)
+
