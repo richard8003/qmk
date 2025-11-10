744243f4540d1819fdb60394086bec721d134be7
richard8003 <richard.johansson.8003@gmail.com>
2025-11-10 08:55:42 +0100

    fubar

--- keyboards/richard/fiffel_pcb_arduino/rules.mk
+++ keyboards/richard/fiffel_pcb_arduino/rules.mk
@@ -15,6 +15,10 @@ AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default
 RGB_MATRIX_ENABLE = no    # Enable keyboard RGB matrix functionality
+UNICODE_ENABLE = yes
+
+NKRO_ENABLE = yes
+
 
 SPLIT_KEYBOARD = yes
 
